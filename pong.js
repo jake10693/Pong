637f05f135dcf60b9ad5acb936be81b39417d5ee
Jacob Carver <55562256+jake10693@users.noreply.github.com>
2020-08-03 16:22:24 -0700

    User piece now functions with the mouse. Computer piece follows along at exactly the same y axis as the ball. Added some minor score functionality. Still needs to have score counter bug fixed and to have the score kept on the canvas board.

--- pong.js
+++ pong.js
@@ -41,42 +41,96 @@ class Ball extends Rect
     }
 }
 
+class Player extends Rect
+{
+    constructor()
+    {
+        super(20, 100);
+        this.score = 0;
+    }
+}
+
+class Pong
+{
+    constructor(canvas)
+    {
+        this._canvas = canvas;
+        this._context = canvas.getContext('2d');
+
+        this.ball = new Ball;
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 50;
+
+        this.ball.vel.x = 100;
+        this.ball.vel.y = 100;
+
+        this.players= [
+            new Player,
+            new Player
+        ];
+
+        this.players[0].pos.x = 40;
+        this.players[1].pos.x = this._canvas.width - 40;
+        this.players.forEach(player => {
+            player.pos.y = this._canvas.height / 2;
+        })
+
+        let lastTime;
+        const callback = (millis) => {
+            if (lastTime) {
+                this.update((millis - lastTime) / 1000);
+            }
+            lastTime = millis;
+            requestAnimationFrame(callback); 
+        };
+        callback();
+    }
+    collide(player, ball)
+    {
+        if (player.left < ball.right && player.right > ball.left &&
+            player.top < ball.bottom && player.bottom > ball.top) {
+                ball.vel.x = -ball.vel.x
+            }
+    }
+    draw()
+    {
+        this._context.fillStyle = '#000';
+        this._context.fillRect(0, 0, 
+            this._canvas.width, this._canvas.height);
+       
+        this.drawRect(this.ball);
+        this.players.forEach(player => this.drawRect(player));
+    }
+    drawRect(rect)
+    {
+        this._context.fillStyle = '#fff';
+        this._context.fillRect(rect.left, rect.top,
+                                 rect.size.x, rect.size.y);
+    }
+    update(dt) 
+    {
+        this.ball.pos.x += this.ball.vel.x * dt;
+        this.ball.pos.y += this.ball.vel.y * dt;
+    
+        if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
+            const playerId = this.ball.vel.x < 0 | 0;
+            console.log(playerId);
+        }
+        if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            this.ball.vel.y = -this.ball.vel.y;
+        }
+
+        this.players[1].pos.y = this.ball.pos.y;
+
+        this.players.forEach(player => this.collide(player, this.ball))
+
+        this.draw();
+    }
+}
+
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
+const pong = new Pong(canvas);
 
-const ball = new Ball;
-ball.pos.x = 100;
-ball.pos.y = 50;
-
-ball.vel.x = 100;
-ball.vel.y = 100;
-
-let lastTime;
-function callback(millis) {
-    if (lastTime) {
-        update((millis - lastTime) / 1000);
-    }
-    lastTime = millis;
-    requestAnimationFrame(callback); 
-}
-
-function update(dt) {
-    ball.pos.x += ball.vel.x * dt;
-    ball.pos.y += ball.vel.y * dt;
-
-    if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
-        ball.vel.x = -ball.vel.x;
-    }
-
-    if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
-        ball.vel.y = -ball.vel.y;
-    }
-
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-   
-    context.fillStyle = '#fff';
-    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-}
-
-callback();
+canvas.addEventListener('mousemove', event => {
+    pong.players[0].pos.y = event.offsetY
+});
