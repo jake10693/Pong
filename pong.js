f5410991a78786767f1d40645fc292be9bbe7f06
Jacob Carver <55562256+jake10693@users.noreply.github.com>
2020-08-04 17:30:36 -0700

    added alerts to instruct the user on how to start the game

--- pong.js
+++ pong.js
@@ -1,6 +1,7 @@
 // Perfect AI
-alert("use the mouse to control the left paddle");
-alert("left click to start the game")
+alert("Use the mouse to control the left paddle!");
+alert("Left click to start the game!");
+alert("Careful! The bot is very good!")
 class Vec 
 {
     constructor(x=0, y=0)
